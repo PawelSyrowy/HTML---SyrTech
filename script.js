0ce2bff791a70df556e930da049e9db05e638a54
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-08-17 16:52:16 +0200

    Revert "nie tak bardzo smooth"
    This reverts commit 1e896d6aefe97846a16ad508e566b0735e0ddce4.

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-﻿// Hamburger menu
-const hamburger = document.querySelector('.hamburger');
+﻿// script.js
+const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-// Reveal on scroll
-const reveals = document.querySelectorAll('.reveal');
-
-function revealOnScroll() {
-    const windowHeight = window.innerHeight;
-    for (let i = 0; i < reveals.length; i++) {
-        const elementTop = reveals[i].getBoundingClientRect().top;
-        const revealPoint = 150;
-        if (elementTop < windowHeight - revealPoint) {
-            reveals[i].classList.add('active');
-        } else {
-            reveals[i].classList.remove('active');
-        }
-    }
-}
-
-window.addEventListener('scroll', revealOnScroll);
