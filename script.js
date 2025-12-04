8028c6895028ede00dbf0f4799a5dbad020171f1
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-12-04 20:48:01 +0100

    niby jakieś przesunięcie ale średnio to widzę

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// dodaj w script.js
+const elems = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+    elems.forEach(e => {
+        if (e.getBoundingClientRect().top < window.innerHeight - 100) {
+            e.classList.add('show');
+        }
+    });
+});
