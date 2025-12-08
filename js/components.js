de1f9e4259c3582e08256a51182f9e1f2b3fa21d
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-12-08 21:04:28 +0100

    jeśli error

--- js/components.js
+++ js/components.js
@@ -1,5 +1,9 @@
 ﻿function loadComponent(id, file) {
     fetch(file)
-        .then(res => res.text())
-        .then(data => document.getElementById(id).innerHTML = data);
+        .then(res => {
+            if (!res.ok) throw new Error("Nie znaleziono komponentu: " + file);
+            return res.text();
+        })
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(err => console.error(err));
 }
